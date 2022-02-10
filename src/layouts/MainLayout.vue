818d10cb6eaf01e9df12fd5920a8fa336bb4a454
Saddam-tech <salokhiddinov0727@gmail.com>
2022-02-10 12:18:08 +0900

    latest changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,29 +3,37 @@
     <div class="main_navbar_wrap">
       <h4>KOSMOS.COM</h4>
       <ul>
-        <li>HOME</li>
-        <li>EQUIPMENT</li>
-        <li>INTELLIGENCE</li>
-        <li>PROGRESS TRACKING</li>
-        <li>WORKOUTS</li>
-        <li>PRICING</li>
+        <li v-for="list in navList" :key="list.id">{{ list.title }}</li>
       </ul>
     </div>
     <div class="main_menu_wrap">
       <div class="first_inner_box">
-        <div class="text_box">
+        <div class="text_box animate__animated animate__fadeIn">
           <h1>SMART HOME GYM</h1>
           <h1>MAKE YOUR</h1>
           <h1>BODY SHAPE</h1>
         </div>
       </div>
-      <div class="second_inner_box"></div>
+      <div class="second_inner_box">
+        <h3>ABOUT US</h3>
+        <div class="modal animate__animated animate__fadeIn">
+          <p>
+            Our company is headquartered in China and was founded in 2020. After
+            several months of hard work, our company has gradually started
+            producing our first product Kosmos I. Our Smart Home Gym are
+            manufactured and sold in China first, then in the countries of the
+            former Soviet Union, and eventually in the wider markets of East
+            Asia, the Middle East and Africa, Europe
+          </p>
+        </div>
+      </div>
     </div>
   </q-layout>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import "animate.css";
 
 export default defineComponent({
   name: "MainLayout",
@@ -33,31 +41,37 @@ export default defineComponent({
     return {
       navList: [
         {
+          id: 1,
           title: "HOME",
           caption: "",
           link: "/home",
         },
         {
+          id: 2,
           title: "EQUIPMENT",
           caption: "",
           link: "/equipment",
         },
         {
+          id: 3,
           title: "INTELLIGENCE",
           caption: "",
           link: "/intelligence",
         },
         {
+          id: 4,
           title: "PROGRESS TRACKING",
           caption: "",
           link: "/progress-tracking",
         },
         {
+          id: 5,
           title: "WORKOUTS",
           caption: "",
           link: "/workouts",
         },
         {
+          id: 6,
           title: "PRICING",
           caption: "",
           link: "#",
