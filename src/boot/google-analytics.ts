3d9882364879a4bef7fc60cbe1ef5bdb52461776
Saddam-tech <salokhiddinov0727@gmail.com>
2022-03-11 14:19:08 +0900

    latest changes

--- src/boot/google-analytics.ts
+++ src/boot/google-analytics.ts
@@ -1,6 +1,8 @@
 import ga from "./analytics.js";
 
 export default ({ router }) => {
+  const sessionId = Math.floor(Math.random() * 1000);
+
   router.afterEach((to, from) => {
     ga.logPage(to.path, to.name, sessionId);
   });
