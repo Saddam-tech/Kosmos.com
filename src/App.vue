dc2521cbeb5fb66bb42ae57d5ee47b9b857e8154
Saddam-tech <salokhiddinov0727@gmail.com>
2022-03-11 11:24:34 +0900

    blocked until paid

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <div style="margin-top: 400px">
+    <h5 style="text-align: center">
+      The site will be available after the charge has been paid!
+    </h5>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
