51ef80ba3cb7fe1bdbb2136c8566020b71df024f
Saddam-tech <salokhiddinov0727@gmail.com>
2022-03-14 13:57:57 +0900

    removed code related to google analytics

--- src/boot/google-analytics.ts
+++
@@ -1,9 +0,0 @@
-import ga from "./analytics.js";
-
-export default ({ router }) => {
-  const sessionId = Math.floor(Math.random() * 1000);
-
-  router.afterEach((to, from) => {
-    ga.logPage(to.path, to.name, sessionId);
-  });
-};
