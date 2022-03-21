7f4e43c575dd789dd2c005b254a840a03ca89f88
Saddam-tech <salokhiddinov0727@gmail.com>
2022-03-21 11:16:40 +0900

    ls

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <div
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    "
+  >
+    <h5 style="text-align: center">The website is not available!</h5>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
